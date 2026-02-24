4f4a57a32fecd9d180201e6c6012047cf2fe8cf0
Ahmad Ramadhan <pencet552@gmail.com>
2026-02-24 08:54:27 +0700

    Percobaan 3

--- Praktikum3/mahasiswaDemo2.java
+++ Praktikum3/mahasiswaDemo2.java
@@ -1,41 +1,42 @@
 package Praktikum3;
 
+import java.util.Scanner;
+
 public class mahasiswaDemo2 {
     public static void main(String[] args) {
         mahasiswa2[] arayofMahasiswa = new mahasiswa2[3];
+        Scanner scanner = new Scanner(System.in);
+        String dummy = null;
 
-        arayofMahasiswa[0] = new mahasiswa2();
-        arayofMahasiswa[0].nim = "254822310";
-        arayofMahasiswa[0].nama = "Prabu Pranotocoro";
-        arayofMahasiswa[0].kelas = "TI-1F";
-        arayofMahasiswa[0].ipk = 3.80f;
+        for (int i = 0; i < arayofMahasiswa.length; i++) {
+            arayofMahasiswa[i] = new mahasiswa2();
+            System.out.println("Masukkan data mahasiswa ke-"+(i+1));  
+            System.out.print("NIM: ");
+            arayofMahasiswa[i].nim = scanner.nextLine();
 
-        arayofMahasiswa[1] = new mahasiswa2();
-        arayofMahasiswa[1].nim = "254738137492";
-        arayofMahasiswa[1].nama = "Ahmad Maulana";
-        arayofMahasiswa[1].kelas = "TI-1H";
-        arayofMahasiswa[1].ipk = 3.54f;
+            System.out.print("Nama:   ");
+            arayofMahasiswa[i].nama = scanner.nextLine();
 
-        arayofMahasiswa[2] = new mahasiswa2();
-        arayofMahasiswa[2].nim = "254313718432";
-        arayofMahasiswa[2].nama = "Purtanto";
-        arayofMahasiswa[2].kelas = "SIB-1D";
-        arayofMahasiswa[2].ipk = 3.92f;
+            System.out.print("Kelas:   ");
+            arayofMahasiswa[i].kelas = scanner.nextLine();
 
-        System.out.println("NIM:     " + arayofMahasiswa[0].nim);
-        System.out.println("Nama:    " + arayofMahasiswa[0].nama);
-        System.out.println("Kelas:   " + arayofMahasiswa[0].kelas);
-        System.out.println("IPK:     " + arayofMahasiswa[0].ipk);
-        System.out.println("===========================================");
-        System.out.println("NIM:     " + arayofMahasiswa[1].nim);
-        System.out.println("Nama:    " + arayofMahasiswa[1].nama);
-        System.out.println("Kelas:   " + arayofMahasiswa[1].kelas);
-        System.out.println("IPK:     " + arayofMahasiswa[1].ipk);
-        System.out.println("===========================================");
-        System.out.println("NIM:     " + arayofMahasiswa[2].nim);
-        System.out.println("Nama:    " + arayofMahasiswa[2].nama);
-        System.out.println("Kelas:   " + arayofMahasiswa[2].kelas);
-        System.out.println("IPK:     " + arayofMahasiswa[2].ipk);
-        System.out.println("===========================================");
+            System.out.print("IPK:   ");
+            dummy = scanner.nextLine();
+            arayofMahasiswa[i].ipk = Float.parseFloat(dummy);
+            System.out.println("========================================");
+
+
+            scanner.nextLine();
+        }
+        for (int i = 0; i < arayofMahasiswa.length; i++) {
+            
+            System.out.println("NIM:     " + arayofMahasiswa[i].nim);
+            System.out.println("Nama:    " + arayofMahasiswa[i].nama);
+            System.out.println("Kelas:   " + arayofMahasiswa[i].kelas);
+            System.out.println("IPK:     " + arayofMahasiswa[i].ipk);
+            System.out.println("===========================================");
+            scanner.close();
+        }
+
     }
 }
