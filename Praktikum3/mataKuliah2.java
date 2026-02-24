f389b88e4c8a4a9911803a223ee68d3cbd2a31f6
Ahmad Ramadhan <pencet552@gmail.com>
2026-02-24 11:07:10 +0700

    Pertanyaan 3

--- Praktikum3/mataKuliah2.java
+++ Praktikum3/mataKuliah2.java
@@ -8,6 +8,13 @@ public class mataKuliah2 {
     public int sks;
     public int jmlJam;
 
+    public void cetakInfo(){
+            System.out.println("Kode:        "+kode );
+            System.out.println("nama:        "+nama);
+            System.out.println("SKS:         "+sks);
+            System.out.println("Jumlah jam:  "+jmlJam);
+            System.out.println("==================================================");
+    }
 
     public void tambahData(Scanner scanner){
         String dummy;
@@ -23,5 +30,4 @@ public class mataKuliah2 {
             jmlJam = Integer.parseInt(dummy);
     }
 
-    
 }
--- Praktikum3/mataKuliahDemo2.java
+++ Praktikum3/mataKuliahDemo2.java
@@ -7,21 +7,17 @@ public class mataKuliahDemo2 {
 
         Scanner scanner = new Scanner(System.in);
         mataKuliah2[] arrayOfMatakuliah = new mataKuliah2[3];
-        String kode = null, nama = null, dummy = null;
-        int sks = 0, jmlJam = 0;
+
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Masukkan data matkul ke-"+(i+1)+ ": ");
             arrayOfMatakuliah[i] = new mataKuliah2();
             arrayOfMatakuliah[i].tambahData(scanner);
+            System.out.println();
         }
         for (int i = 0; i < 3; i++) {
-            System.out.println("data mata kuliah ke-"+ (i+1));
-            System.out.println("Kode:        "+arrayOfMatakuliah[i].kode );
-            System.out.println("nama:        "+arrayOfMatakuliah[i].nama);
-            System.out.println("SKS:         "+arrayOfMatakuliah[i].sks);
-            System.out.println("Jumlah jam:  "+arrayOfMatakuliah[i].jmlJam);
-            System.out.println("==================================================");
+            System.out.println("Data mata kuliah ke-"+(i+1));
+            arrayOfMatakuliah[i].cetakInfo();
         }
     }
 }
