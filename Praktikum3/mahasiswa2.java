2a57ae448deb9a599c73c3b8ec0dffc8f61bb515
Ahmad Ramadhan <pencet552@gmail.com>
2026-02-24 09:18:22 +0700

    Modifikasi Percobaan 3

--- Praktikum3/mahasiswa2.java
+++ Praktikum3/mahasiswa2.java
@@ -5,4 +5,14 @@ public class mahasiswa2 {
     public String nama;
     public String kelas;
     public float ipk;
+    
+    public static void ctkInf(mahasiswa2[] arayofMahasiswa ,int pjg){
+      for (int i = 0; i < pjg; i++) {
+            System.out.println("NIM:     " + arayofMahasiswa[i].nim);
+            System.out.println("Nama:    " + arayofMahasiswa[i].nama);
+            System.out.println("Kelas:   " + arayofMahasiswa[i].kelas);
+            System.out.println("IPK:     " + arayofMahasiswa[i].ipk);
+            System.out.println("===========================================");
+      }  
+    }
 }
